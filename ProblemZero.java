6ef8da59314c19fc8dcec24efcaa20096973c8a8
meggie123 <meghanabura@gmail.com>
2022-05-13 21:34:19 +0530

    Welcome To The Snake And Ladder Simulator

---
+++ ProblemZero.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class ProblemZero {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake And Ladder Simulator");
+    }
+}
