b56aec0f2b51476a23f728de5bfd32cdcc6cc0b2
meggie123 <meghanabura@gmail.com>
2022-05-13 21:40:40 +0530

    Checking: Start Position 0

---
+++ ProblemOne.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.SnakeAndLadder;
+
+public class ProblemOne {
+    public static void main(String [] args){
+
+        System.out.println("Welcome To Snake And Ladder Simulator");
+        int startPosition = 0;
+        System.out.println("Starting Position is :" + startPosition);
+    }
+}
